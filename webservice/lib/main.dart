3169d2b2a68f0f46842451a3b5cf3bbb74e14ef6
jorajipo <jorajipo@gmail.com>
2020-07-23 19:18:31 -0500

    Agregamos proyecto de grid con webservices

--- webservice/lib/main.dart
+++ webservice/lib/main.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
-Future<Post> fetchPost() async {
+Future<Movie> fetchPost() async {
   final response =
-  await http.get('https://jsonplaceholder.typicode.com/posts/1');
+  await http.get('http://www.omdbapi.com/?i=tt3896198&apikey=e58bb080');
 
   if (response.statusCode == 200) {
     // Si la llamada al servidor fue exitosa, analiza el JSON
-    return Post.fromJson(json.decode(response.body));
+    return Movie.fromJson(json.decode(response.body));
   } else {
     // Si la llamada no fue exitosa, lanza un error.
     throw Exception('Failed to load post');
@@ -18,21 +18,23 @@ Future<Post> fetchPost() async {
 }
 
 //clase que recibe el contenido del Json y los presenta como propiedades separadas
-class Post {
-  final int userId;
-  final int id;
+class Movie {
+  final String year;
+  final String genre;
   final String title;
-  final String body;
+  final String rate;
+  final String runtime;
 
-  Post({this.userId, this.id, this.title, this.body});
+  Movie({this.year, this.genre, this.title, this.rate, this.runtime});
 
   //En esta parte copiamos el json a cada una de las propiedades del objeto
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      year: json['Year'],
+      genre: json['Genre'],
+      title: json['Title'],
+      rate: json['Rated'],
+      runtime: json['Runtime'],
     );
   }
 }
@@ -40,7 +42,7 @@ class Post {
 void main() => runApp(MyApp(post: fetchPost()));
 
 class MyApp extends StatelessWidget {
-  final Future<Post> post;
+  final Future<Movie> post;
 
   MyApp({Key key, this.post}) : super(key: key);
 
@@ -56,11 +58,12 @@ class MyApp extends StatelessWidget {
           title: Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Post>(
+          child: FutureBuilder<Movie>(
             future: post,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Text("User id: ${snapshot.data.userId}\n id: ${snapshot.data.id} \n: title: ${snapshot.data.title} \n body: ${snapshot.data.body}");
+                return Text("Película:\n Título:  ${snapshot.data.title}\n Duración: ${snapshot.data.runtime} "
+                    "\n: Género: ${snapshot.data.genre} \n Año: ${snapshot.data.year} \n Clasificación ${snapshot.data.rate}");
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
---
+++ webservice_grid/android/app/src/main/kotlin/com/jorajipo/webservicegrid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jorajipo.webservicegrid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
